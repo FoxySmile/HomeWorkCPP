bf701d4094ca6cc1ba449ce2f61af31e05586a3e
FoxySmile <142438460+FoxySmile@users.noreply.github.com>
2024-02-24 01:45:56 +0300

    Add files via upload

--- 15.02.24/DefaultCPP.cpp
+++ 15.02.24/DefaultCPP.cpp
@@ -34,7 +34,132 @@ public:
 		count++;
 	}
 
+	void insert(T el, int index) {
+		if (index == 0) {
+			push(el);
+			return;
+		}
+		if (index > count || index < 0) {
+			return;
+		}
+		Node<T>* buf = head;
+		Node<T>* newNode;
+		if (index <= count / 2) {
+			for (int i = 0; i < index - 1; i++) {
+				buf = buf->next;
+			}
+			newNode = new Node<T>{ el, buf->next, buf };
+			buf->next->prev = newNode;
+			buf->next = newNode;
+		}
+		else {
+			buf = tail;
+			for (int i = 0; i < count - index - 1; i++) {
+				buf = buf->prev;
+			}
+			newNode = new Node<T>{ el, buf, buf->prev };
+			buf->prev->next = newNode;
+			buf->prev = newNode;
+		}
+		count++;
+	}
 
+	void removeElementByIndex(int index) {
+		Node<T>* buf = head;
+		Node<T>* remEl;
+		if (index <= count / 2) {
+			if (index == 0) {
+				dropHead();
+				return;
+			}
+			for (int i = 0; i < index - 1; i++) {
+				buf = buf->next;
+			}
+			remEl = buf->next;
+			buf->next = buf->next->next;
+			buf->next->prev = buf;
+			delete[]remEl;
+		}
+		else {
+			buf = tail;
+			if (index == count) {
+				dropTail();
+				return;
+			}
+			for (int i = 0; i < count - index - 1; i++) {
+				buf = buf->prev;
+			}
+			remEl = buf->prev;
+			buf->prev = buf->prev->prev;
+			buf->prev->next = buf;
+			delete[]remEl;
+		}
+		count--;
+	}
+
+	void removeElementsByIndex(int index, int num) {
+		Node<T>* buf = head;
+		Node<T>* remEl;
+		if (num > count - 1) {
+			return;
+		}
+		if (index <= count / 2) {
+			if (index == 0) {
+				for (int i = 0; i < num; i++) {
+					dropHead();
+				}
+				return;
+			}
+			for (int i = 0; i < index - 1; i++) {
+				buf = buf->next;
+			}
+			for (int i = index; i < index + num + 1; i++) {
+				remEl = buf->next;
+				buf->next = buf->next->next;
+				buf->next->prev = buf;
+				delete[]remEl;
+				count--;
+			}
+		}
+		else {
+			buf = tail;
+			if (index == count) {
+				for (int i = 0; i < num; i++) {
+					dropTail();
+				}
+				return;
+			}
+			for (int i = 0; i < index - 1; i++) {
+				buf = buf->prev;
+			}
+			for (int i = index; i < index + num + 1; i++) {
+				remEl = buf->prev;
+				buf->prev = buf->prev->prev;
+				buf->prev = buf;
+				delete[]remEl;
+				count--;
+			}
+		}
+	}
+
+	void dropHead() {
+		if (count == 0) {
+			return;
+		}
+		Node<T>* buf = head;
+		head = head->next;
+		head->prev = nullptr;
+		count--;
+		delete[] buf;
+	}
+
+	void dropTail() {
+		Node<T>* buf = tail;
+		tail = tail->prev;
+		tail->next = nullptr;
+		count--;
+		delete[] buf;
+	}
 
 	void showHead() {
 		Node<T>* buf = head;
@@ -58,9 +183,11 @@ public:
 int main() {
 	srand(time(NULL));
 	List<int> list;
-	for (int i = 0; i < 2; i++) {
+	for (int i = 0; i < 10; i++) {
 		list.push(i);
 	}
+	list.showHead();
+	list.removeElementsByIndex(3, 2);
 	list.showHead();
 	list.showTail();
 }
